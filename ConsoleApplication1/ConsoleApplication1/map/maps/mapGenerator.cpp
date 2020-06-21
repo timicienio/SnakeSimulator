0c04969f28d813243db5f2dffb2363bfc6237cea
desk2000 <52657198+desk2000@users.noreply.github.com>
2020-06-21 20:54:51 +0800

    Map generator update

--- ConsoleApplication1/ConsoleApplication1/map/maps/mapGenerator.cpp
+++ ConsoleApplication1/ConsoleApplication1/map/maps/mapGenerator.cpp
@@ -1,17 +1,16 @@
-#include <iostream>
+// #include <Windows.h>
 #include <fstream>
+#include <iostream>
 #include <random>
+#include <string>
 #include <utility>
 #include <vector>
-#include <string>
-#include <Windows.h>
-
 
 using namespace std;
 
-
 const pair<int, int> MAP_SIZE = make_pair(50, 50); // including boundary
-const int NUM_FOOD = 3; // food each round
+const int NUM_FOOD = 2; // food each round
+const int SCORE_RANGE = 3; // range of score of each food, from 1 to SCORE_RANGE
 
 int main()
 {
@@ -39,53 +38,42 @@ int main()
     barriers.push_back(make_pair(7, 20));
     barriers.push_back(make_pair(8, 33));
 
-
     vector<vector<int>> tempMap;
 
-    for (size_t j = 0; j < MAP_SIZE.second; j++)
-    {
+    for (size_t j = 0; j < MAP_SIZE.second; j++) {
         tempMap.push_back(vector<int>());
-        if (j == 0 || j == MAP_SIZE.second - 1)
-        {
-            for (size_t k = 0; k < MAP_SIZE.first; k++)
-            {
+        if (j == 0 || j == MAP_SIZE.second - 1) {
+            for (size_t k = 0; k < MAP_SIZE.first; k++) {
                 tempMap[j].push_back(-1);
             }
         }
 
-        else
-        {
-            for (size_t k = 0; k < MAP_SIZE.second; k++)
-            {
-                if (k == 0 || k == MAP_SIZE.first - 1)
-                {
+        else {
+            for (size_t k = 0; k < MAP_SIZE.second; k++) {
+                if (k == 0 || k == MAP_SIZE.first - 1) {
                     tempMap[j].push_back(-1);
-                }
-                else
-                {
+                } else {
                     tempMap[j].push_back(0);
                 }
             }
         }
     }
 
-    for (size_t i = 0; i < barriers.size(); i++)
-    {
+    for (size_t i = 0; i < barriers.size(); i++) {
         tempMap[barriers[i].first][barriers[i].second] = -1;
     }
 
-    std::random_device rd;
-    std::default_random_engine gen = std::default_random_engine(rd());
-    std::uniform_int_distribution<int> dis(1,48);
-    std::uniform_int_distribution<int> foodScoreDis(1, 10);
+    random_device rd;
+    minstd_rand gen = minstd_rand(rd());
+    uniform_int_distribution<int> dis(1, MAP_SIZE.first - 2);
+    uniform_int_distribution<int> dis2(1, MAP_SIZE.first - 2);
+    uniform_int_distribution<int> foodScoreDis(1, SCORE_RANGE);
 
     string filename = "";
-    for (size_t i = 0; i < 100; i++)
-    {
+    for (size_t i = 0; i < 100; i++) {
         vector<vector<int>> map = tempMap;
         filename = "map_" + to_string(i + 101);
         cout << "writing " << filename << "\n";
-
 
         // if (!fp)
         // {
@@ -96,17 +84,14 @@ int main()
         {
             bool success = false;
             pair<int, int> foodPos;
-            for (size_t j = 0; j < NUM_FOOD; j++)
-            {
+            for (size_t j = 0; j < NUM_FOOD; j++) {
                 success = false;
                 cout << j << "\n";
-                while (!success)
-                {
-                    foodPos = make_pair(dis(gen), dis(gen));
+                while (!success) {
+                    foodPos = make_pair(dis(gen), dis2(gen));
                     cout << "try put food at " << foodPos.first << "," << foodPos.second << "\n";
 
-                    if (map[foodPos.first][foodPos.second] == 0)
-                    {
+                    if (map[foodPos.first][foodPos.second] == 0) {
                         map[foodPos.first][foodPos.second] = foodScoreDis(gen);
                         cout << "put food at " << foodPos.first << "," << foodPos.second << "\n";
                         success = true;
@@ -118,26 +103,20 @@ int main()
         fstream fp;
         fp.open(filename, ios::out);
 
-        for (size_t j = 0; j < MAP_SIZE.second; j++)
-        {
-            for (size_t k = 0; k < MAP_SIZE.first; k++)
-            {
-                if (!k == 0)
-                {
+        for (size_t j = 0; j < MAP_SIZE.second; j++) {
+            for (size_t k = 0; k < MAP_SIZE.first; k++) {
+                if (!k == 0) {
                     fp << " ";
                 }
                 fp << to_string(map[j][k]);
             }
-            if(j != MAP_SIZE.second - 1)
-            {
-                fp << "\n";
-            }
+            if (j != MAP_SIZE.second - 1) fp << "\n";
         }
 
         fp.close();
     }
 
-    system("pause");
+    // system("pause");
 
     return 0;
 }
